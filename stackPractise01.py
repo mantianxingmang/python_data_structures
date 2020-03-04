6e261a3c3136ac67f90a042995e3db1d545932a9
zy <1375020779@qq.com>
2020-03-04 20:21:37 +0800

    ({[符号匹配

--- stackPractise01.py
+++ stackPractise01.py
@@ -49,9 +49,44 @@
 
 
 # （）匹配
+# from pythonds.basic.stack import Stack
+
+# #  symbolString  假设 “（（））”
+# def parChecker(symbolString):
+#     s = Stack()
+#     flag = True
+#     index = 0
+
+#     while index < len(symbolString) and flag:
+#         symbol = symbolString[index]
+#         if symbol == "(":
+#             s.push(symbol)
+#         else:
+#             if s.isEmpty():
+#                 flag = False
+#             else:
+#                 s.pop()
+
+#         index = index + 1
+
+#     if flag and s.isEmpty():
+#         return True
+#     else:
+#         return False
+
+
+# print(parChecker('(())'))
+# print(parChecker('((())'))
+
+
+
+#  {[()]}   ( [ )]
+# 每一个开始的符号被压入栈，等待匹配结果
+# 当出现结束符号的时候，必须检查栈顶部的开始符号是什么类型，如果两个符号不匹配，则字符串不匹配
+# 整个字符串处理完并且栈为空，则字符串匹配
+
 from pythonds.basic.stack import Stack
 
-#  symbolString  假设 “（（））”
 def parChecker(symbolString):
     s = Stack()
     flag = True
@@ -59,21 +94,27 @@ def parChecker(symbolString):
 
     while index < len(symbolString) and flag:
         symbol = symbolString[index]
-        if symbol == "(":
+
+        if symbol in "([{":     # [{
             s.push(symbol)
         else:
             if s.isEmpty():
                 flag = False
             else:
-                s.pop()
+                top = s.pop()   # {
+                start = '([{'  # 2
+                end = ')]}'   # 1
 
-        index = index + 1
+                if not start.index(top) == end.index(symbol):
+                    flag = False
+        index = index + 1 
+
 
     if flag and s.isEmpty():
         return True
     else:
         return False
 
-
-print(parChecker('(())'))
-print(parChecker('((())'))
+print(parChecker('{[()]}'))
+print(parChecker('{{([][])}()}'))
+print(parChecker('[{()]'))
