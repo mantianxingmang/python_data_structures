daa993d00c8655a04869080db5fe48aca982959c
zy <1375020779@qq.com>
2020-03-06 10:04:03 +0800

    递归三大定律

---
+++ DS2-recursion/listsum.py
@@ -0,0 +1,58 @@
+'''
+[1,3,5,7,9]
+'''
+def listSum(numList):
+    sum = 0
+    for i in numList:
+        sum = sum + i
+    
+    return sum
+
+print(listSum([1,3,5,7,9]))
+
+
+#不能使用while或者for
+#使用小学的内容：连加
+#（1+3） = （4+5）= （9+7）= 16+9 = 25
+#（1+（3+（5+（7+9））））
+#     （1+（3+（5+16）））
+#           （1+（3+21））
+#                 （1+24）
+#                      25
+
+'''
+    listSum2([1,3,5,7,9])
+= 1 + listSum2([5,7,9])
+= 3 + listSum2([5,7,9])
+= 5 + listSum2([7,9])
+= 7 + listSum2([9])
+'''
+def listSum2(numList):
+    if len(numList) == 1:
+        return numList[0]
+    else:
+        return numList[0] + listSum2(numList[1:])
+
+print(listSum2([1,3,5,7,9]))
+
+
+
+
+
+
+def toStr(n,base):
+    str1 = '0123456789ABCDEF'
+
+    if n < base:
+        return str1[n]
+    else:
+        return toStr(n//base,base) + str1[n%base]
+
+
+print(toStr(100,10)) 
+
+'''
+toStr(10,10) + '0'
+toStr(1,10) + '0'
+'1'
+'''
